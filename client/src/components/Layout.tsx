060151bf637cde66b87ebee7e7acb3be49b984a1
seleema <sel23nice@gmail.com>
2026-06-10 11:27:26 +0530

    side bar

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,8 +1,18 @@
 import { useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLoaderData, useLocation } from 'react-router-dom';
 import Sidebar from './Sidebar';
+import { MenuIcon } from 'lucide-react';
 
+const pageTitles: Record<string, string>=
+{
+    "/dashboard" : "Dashboard",
+    "/accounts": "Social Accounts",
+    "/schedule": "Post Scheduler",
+    "/ai-composer": "AI Composer",
+}
 const Layout = () => {
+    const location= useLocation()
+    const title=pageTitles[location.pathname] || "SocialAI";
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   return (
@@ -19,16 +29,27 @@ const Layout = () => {
         setIsOpen={setIsMobileMenuOpen}
       />
 
-      <div className="flex-1 flex flex-col">
+      <div className='flex-1 flex flex-col overflow-hidden'>
         {/* Top Bar */}
-        <header>
+        
+        <header className='h-16 bg-white border-b border-slate-200 flex items-center px-4 md:px-8 gap-4'>
           {/* Navbar content */}
-        </header>
+       
+           
+            <button className='md:hidden p-2 ml-2 text-slate-500' onClick={()=>setIsMobileMenuOpen(true)}>
+                <MenuIcon className='size-6'/>
+            </button>
+        <div>
+            <h1 className='text-slate-900'>{title}</h1>
+            <p>Manage and automate your social presence</p>
+        </div>
+             </header>
 
-        <main className="flex-1 overflow-auto p-4 sm:p-6 md:p-8 xl:p-12">
+        <main className='flex-1 overflow-auto p-4 sm:p-6 md:p-8 xl:p-12'>
           <Outlet />
         </main>
-      </div>
+     
+        </div>
     </div>
   );
 };
--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const Sidebar = () => {
+const Sidebar = ({isOpen,setIsOpen} : {isOpen: boolean,setIsOpen: (val:boolean)=>void})=>{
   return (
-    <div>Sidebar</div>
+    <div className={'fixed inset-y-0 left-0 z-50 w-64 bg-white border-r border-slate-200 flex flex-col h-full transform transition-transform duration-200 ease-in-out md:relative md:translate-x-0 ${isOpen ?"translate-x-0":"-translate-x-full"}'}>
+        
+    </div>
   )
 }
 
